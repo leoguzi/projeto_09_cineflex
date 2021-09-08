ed7a8893546760bd98f588b6b72e8fe907c33dda
Sidney Outeiro <sid@dcc.ufrj.br>
2021-09-08 12:04:40 -0300

    preparando para o review

--- src/main/Card.js
+++
@@ -1,8 +0,0 @@
-import "./card.css";
-export default function Card({ posterURL, title }) {
-  return (
-    <div className="card">
-      <img src={posterURL} alt={title}></img>
-    </div>
-  );
-}
